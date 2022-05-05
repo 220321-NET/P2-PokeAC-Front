a80b2679a26f31b30120dfea99f9364aee8ab622
Leo Doan <doanleo@yahoo.com>
2022-05-05 11:30:36 -0400

    Co-authored-by: Matthew Cooper <matthewcooper228@users.noreply.github.com> Co-authored-by: reyjvergara <reyjvergara@users.noreply.github.com> Co-authored-by: blchilds55 <blchilds55@users.noreply.github.com>

--- NGPokeAC/src/app/app.module.ts
+++ NGPokeAC/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameBoardComponent } from './game-board/game-board.component';
-import { FormsModule} from '@angular/forms'
+import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+
 
 
 @NgModule({
